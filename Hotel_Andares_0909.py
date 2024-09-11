a608ab270c7845731cc1797545d4d46e3395d076
msilva186 <45169230+msilva186@users.noreply.github.com>
2024-09-11 15:25:32 -0300

    Add files via upload

---
+++ Hotel_Andares_0909.py
@@ -0,0 +1,21 @@
+#Retorna andares do Hotel.
+andar_hotel = 0
+for i in range(20 ):
+  andar_hotel = andar_hotel + 1
+  if (andar_hotel == 13):
+    continue
+  if (andar_hotel == 21):
+    break
+  print("O número do andar é:", andar_hotel)
+  
+  -------------------------------------------------
+#Retorna andares do Hotel em ordem decrescente (20, 19, 18 ...):
+andar_hotel = 21
+while andar_hotel != 1:  
+    andar_hotel = andar_hotel -1
+    if (andar_hotel == 13):
+      continue
+    if (andar_hotel == 0):
+      break 
+    print("O número do andar é:", andar_hotel) 
+    ---------------------------------------------------------------------------------
