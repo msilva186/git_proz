a8c8d28aae24f540ecb6d7a929b01c63e8497223
msilva186 <45169230+msilva186@users.noreply.github.com>
2024-09-16 16:39:56 -0300

    Update Ano_Nascimento_1609.py

--- Ano_Nascimento_1609.py
+++ Ano_Nascimento_1609.py
@@ -4,7 +4,7 @@ while True: # enquanto não digitar um valor válido, pede que digite novamente
     try:
         ano_nascimento = int(input('Digite ano de Nascimento(entre 1922 e 2021):'))
         if 1922 <= ano_nascimento <= 2021:
-            print("Sua idade é: ", 2024 - ano_nascimento)
+            print(f"O usuário {nome} completou ou completará {2022 - ano_nascimento} em 2022: ")
             break # valor válido, sai do while
         # se não entrou no if acima, o valor é inválido
         print('Ano deve ser entre 1922 e 2021, formato: YYYY: ')
