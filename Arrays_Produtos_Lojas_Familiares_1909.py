f8130c7b4cf21eb571bf07692b2c44c4e4323eb4
msilva186 <45169230+msilva186@users.noreply.github.com>
2024-09-19 16:30:30 -0300

    Add files via upload

---
+++ Arrays_Produtos_Lojas_Familiares_1909.py
@@ -0,0 +1,7 @@
+# Array com produtos de uma loja de alimentos
+loja_Roupa = ["Camiseta", "Blusa", "Saia", "Calça", "Jaqueta"]
+print(loja_Roupa) 
+
+# Array com anos de nascimento de familiares e amigos
+ano_Nascimento = [1986, 1988, 1950, 1981, 2001]
+print(ano_Nascimento)
