9835985d6345207d0c1cb5c6b7bd9f2d8217b199
msilva186 <45169230+msilva186@users.noreply.github.com>
2024-09-20 22:01:16 -0300

    Add files via upload

---
+++ Lista_produtos_2.py
@@ -0,0 +1,14 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+lista_produtos[1] = 'rímel'
+print(lista_produtos)
+
+lista_produtos[4] = 'cremes hidratantes'
+print(lista_produtos)
+
+lista_produtos.pop()
+print(lista_produtos)
+
+lista_produtos.append('Desodorante') 
+lista_produtos.append('listerine') 
+print(lista_produtos) 
