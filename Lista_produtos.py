1c995ecd939f7a4f2041b7a26899b2f674b2f79d
msilva186 <45169230+msilva186@users.noreply.github.com>
2024-09-20 21:22:55 -0300

    Add files via upload

---
+++ Lista_produtos.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+for i in range(len(lista_produtos)):
+  print(f'Temos \033[1m {lista_produtos[i]}\033[0m à venda!')
